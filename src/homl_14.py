bbb0e9d764470b90074f6451774d85f5adc89d36
RM <rmorris.code@gmail.com>
2022-10-13 13:04:05 -0700

    read transfer learning section

--- src/homl_14.py
+++ src/homl_14.py
@@ -122,4 +122,42 @@ images_resized = tf.image.resize(images, [224, 224]) # does not preserve aspect 
 inputs = keras.applications.resnet50.preprocess_input(images_resized * 255) # preprocess() assumes 0-255
 y_proba = model.predict(inputs)
 
+## Pretrained Models for Transfer Learning p. 481
+import tensorflow_datasets as tfds
+dataset, info = tfds.load('tf_flowers', as_supervised=True, with_info=True)
+dataset_size = info.splits['train'].num_examples
+class_names = info.features['label'].names
+n_classes = info.features['label'].num_classes
+test_split, valid_split, train_split = tfds.Split.TRAIN.subsplit([10, 15, 75])
+
+test_set = tfds.load('tf_flowers', split=test_split, as_supervised=True)
+valid_set = tfds.load('tf_flowers', split=valid_split, as_supervised=True)
+train_set = tfds.load('tf_flowers', split=train_split, as_supervised=True)
+
+def preprocess(image, label):
+    resized_image = tf.image.resize(image, [224, 224])
+    final_image = keras.applications.xception.preprocess_input(resized_image)
+    return final_image, label
+
+batch_size = 32
+train_set = train_set.shuffle(1000)
+train_set = train_set.map(preprocess).batch(batch_size).prefetch(1)
+valid_set = valid_set.map(preprocess).batch(batch_size).prefetch(1)
+test_set = test_set.map(preprocess).batch(batch_size).prefetch(1)
+
+# can use keras.preprocessing.image.ImageDataGenerator to create random crops and flips
+
+# using an xception model, trained on ImageNet
+base_model = keras.applications.xception.Xception(wewights='imagenet',
+                                                  inclue_top=False)
+avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
+output = keras.layers.Dense(n_classes, activation='softmax')(avg)
+model = keras.Model(inputs=base_model.input, outputs=output)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+optimizer = keras.optimizers.SGD(lr=.2, momentum=.9, decay=.01)
+model.compile(...)
+history = model.fit(...)
 
